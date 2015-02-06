b6675b517dc1fd91e960bd17744ec169439a0ce9
si-yao <siyao.li.szeyiu@gmail.com>
2015-02-06 00:36:08 -0500

    provided files

---
+++ Assignment1/solutionsA.py
@@ -0,0 +1,93 @@
+#a function that calculates unigram, bigram, and trigram probabilities
+#brown is a python list of the sentences
+#this function outputs three python dictionaries, where the key is a tuple expressing the ngram and the value is the log probability of that ngram
+#make sure to return three separate lists: one for each ngram
+def calc_probabilities(brown):
+    unigram_p = {}
+    bigram_p = {}
+    trigram_p = {}
+    return unigram_p, bigram_p, trigram_p
+
+#each ngram is a python dictionary where keys are a tuple expressing the ngram, and the value is the log probability of that ngram
+def q1_output(unigrams, bigrams, trigrams):
+    #output probabilities
+    outfile = open('A1.txt', 'w')
+    for unigram in unigrams:
+        outfile.write('UNIGRAM ' + unigram[0] + ' ' + str(unigrams[unigram]) + '\n')
+    for bigram in bigrams:
+        outfile.write('BIGRAM ' + bigram[0] + ' ' + bigram[1]  + ' ' + str(bigrams[bigram]) + '\n')
+    for trigram in trigrams:
+        outfile.write('TRIGRAM ' + trigram[0] + ' ' + trigram[1] + ' ' + trigram[2] + ' ' + str(trigrams[trigram]) + '\n')
+    outfile.close()
+    
+#a function that calculates scores for every sentence
+#ngram_p is the python dictionary of probabilities
+#n is the size of the ngram
+#data is the set of sentences to score
+#this function must return a python list of scores, where the first element is the score of the first sentence, etc. 
+def score(ngram_p, n, data):
+    scores = []
+    return scores
+
+
+#this function outputs the score output of score()
+#scores is a python list of scores, and filename is the output file name
+def score_output(scores, filename):
+    outfile = open(filename, 'w')
+    for score in scores:
+        outfile.write(str(score) + '\n')
+    outfile.close()
+
+
+#this function scores brown data with a linearly interpolated model
+#each ngram argument is a python dictionary where the keys are tuples that express an ngram and the value is the log probability of that ngram
+#like score(), this function returns a python list of scores
+def linearscore(unigrams, bigrams, trigrams, brown):
+    scores = []
+    return scores
+
+def main():
+    #open data
+    infile = open('Brown_train.txt', 'r')
+    brown = infile.readlines()
+    infile.close()
+
+    #calculate ngram probabilities (question 1)
+    unigrams, bigrams, trigrams = calc_probabilities(brown)
+
+    #question 1 output
+    q1_output(unigrams, bigrams, trigrams)
+
+    #score sentences (question 2)
+    uniscores = score(unigrams, 1, brown)
+    biscores = score(bigrams, 2, brown)
+    triscores = score(trigrams, 3, brown)
+
+    #question 2 output
+    score_output(uniscores, 'A2.uni.txt')
+    score_output(biscores, 'A2.bi.txt')
+    score_output(triscores, 'A2.tri.txt')
+
+    #linear interpolation (question 3)
+    linearscores = linearscore(unigrams, bigrams, trigrams, brown)
+
+    #question 3 output
+    score_output(linearscores, 'A3.txt')
+
+    #open Sample1 and Sample2 (question 5)
+    infile = open('Sample1.txt', 'r')
+    sample1 = infile.readlines()
+    infile.close()
+    infile = open('Sample2.txt', 'r')
+    sample2 = infile.readlines()
+    infile.close() 
+
+    #score the samples
+    sample1scores = linearscore(unigrams, bigrams, trigrams, sample1)
+    sample2scores = linearscore(unigrams, bigrams, trigrams, sample2)
+
+    #question 5 output
+    score_output(sample1scores, 'Sample1_scored.txt')
+    score_output(sample2scores, 'Sample2_scored.txt')
+
+if __name__ == "__main__": main()
---
+++ Assignment1/solutionsB.py
@@ -0,0 +1,160 @@
+import sys
+import nltk
+import math
+
+#this function takes the words from the training data and returns a python list of all of the words that occur more than 5 times
+#wbrown is a python list where every element is a python list of the words of a particular sentence
+def calc_known(wbrown):
+    knownwords = []
+
+    return knownwords
+
+#this function takes a set of sentences and a set of words that should not be marked '_RARE_'
+#brown is a python list where every element is a python list of the words of a particular sentence
+#and outputs a version of the set of sentences with rare words marked '_RARE_'
+def replace_rare(brown, knownwords):
+    rare = []
+    return rare
+
+#this function takes the ouput from replace_rare and outputs it
+def q3_output(rare):
+    outfile = open("B3.txt", 'w')
+
+    for sentence in rare:
+        outfile.write(' '.join(sentence)[2:-1] + '\n')
+    outfile.close()
+
+#this function takes tags from the training data and calculates trigram probabilities
+#tbrown (the list of tags) should be a python list where every element is a python list of the tags of a particular sentence
+#it returns a python dictionary where the keys are tuples that represent the trigram, and the values are the log probability of that trigram
+def calc_trigrams(tbrown):
+    qvalues = {}
+    return qvalues
+
+#this function takes output from calc_trigrams() and outputs it in the proper format
+def q2_output(qvalues):
+    #output 
+    outfile = open("B2.txt", "w")
+    for trigram in qvalues:
+        output = " ".join(['TRIGRAM', trigram[0], trigram[1], trigram[2], str(qvalues[trigram])])
+        outfile.write(output + '\n')
+    outfile.close()
+
+#this function calculates emission probabilities and creates a list of possible tags
+#the first return value is a python dictionary where each key is a tuple in which the first element is a word 
+#and the second is a tag and the value is the log probability of that word/tag pair
+#and the second return value is a list of possible tags for this data set
+#wbrown is a python list where each element is a python list of the words of a particular sentence
+#tbrown is a python list where each element is a python list of the tags of a particular sentence
+def calc_emission(wbrown, tbrown):
+    evalues = {}
+    taglist = []
+    return evalues, taglist
+
+#this function takes the output from calc_emissions() and outputs it
+def q4_output(evalues):
+    #output
+    outfile = open("B4.txt", "w")
+    for item in evalues:
+        output = " ".join([item[0], item[1], str(evalues[item])])
+        outfile.write(output + '\n')
+    outfile.close()
+
+
+#this function takes data to tag (brown), possible tags (taglist), a list of known words (knownwords), 
+#trigram probabilities (qvalues) and emission probabilities (evalues) and outputs a list where every element is a string of a 
+#sentence tagged in the WORD/TAG format 
+#brown is a list where every element is a list of words
+#taglist is from the return of calc_emissions()
+#knownwords is from the the return of calc_knownwords()
+#qvalues is from the return of calc_trigrams
+#evalues is from the return of calc_emissions() 
+#tagged is a list of tagged sentences in the format "WORD/TAG". Each sentence is a string, not a list of tokens.
+def viterbi(brown, taglist, knownwords, qvalues, evalues):
+    tagged = []
+    return tagged
+
+#this function takes the output of viterbi() and outputs it
+def q5_output(tagged):
+    outfile = open('B5.txt', 'w')
+    for sentence in tagged:
+        outfile.write(sentence)
+    outfile.close()
+
+#this function uses nltk to create the taggers described in question 6
+#brown is the data to be tagged
+#tagged is a list of tagged sentences. Each sentence is in the WORD/TAG format and is a string rather than a list of tokens.
+def nltk_tagger(brown):
+    tagged = []
+    return tagged
+
+def q6_output(tagged):
+    outfile = open('B4.txt', 'w')
+
+    for sentence in tagged:
+        outfile.write(sentence)
+    outfile.close()
+
+#a function that returns two lists, one of the brown data (words only) and another of the brown data (tags only) 
+def split_wordtags(brown_train):
+    wbrown = []
+    tbrown = []
+    return wbrown, tbrown
+
+def main():
+    #open Brown training data
+    infile = open("Brown_tagged_train.txt", "r")
+    brown_train = infile.readlines()
+    infile.close()
+
+    #split words and tags, and add start and stop symbols (question 1)
+    wbrown, tbrown = split_wordtags(brown_train)
+           
+    #calculate trigram probabilities (question 2)
+    qvalues = calc_trigrams(tbrown)
+    
+    #question 2 output
+    q2_output(qvalues)
+
+    #calculate list of words with count > 5 (question 3)
+    knownwords = calc_known(wbrown)
+
+    #get a version of wbrown with rare words replace with '_RARE_' (question 3)
+    wbrown_rare = replace_rare(wbrown, knownwords)
+
+    #question 3 output
+    q3_output(wbrown_rare)
+
+    #calculate emission probabilities (question 4)
+    evalues, taglist = calc_emission(wbrown_rare, tbrown)
+
+    #question 4 output
+    q4_output(evalues)
+
+    #delete unneceessary data
+    del brown_train
+    del wbrown
+    del tbrown
+    del wbrown_rare
+
+    #open Brown development data (question 5)
+    infile = open("Brown_dev.txt", "r")
+    brown_dev = infile.readlines()
+    infile.close()
+
+
+    #replace rare words in brown_dev (question 5)
+    brown_dev_rare = replace_rare(brown_dev, knownwords)
+
+    #do viterbi on brown_dev (question 5)
+    viterbi_tagged = viterbi(brown_dev, taglist, knownwords, qvalues, evalues)
+
+    #question 5 output
+    q5_output(viterbi_tagged)
+
+    #do nltk tagging here
+    nltk_tagged = nltk_tagger(brown_dev)
+    
+    #question 6 output
+    q6_output(nltk_tagged)
+if __name__ == "__main__": main()
