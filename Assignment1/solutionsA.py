ce986c9712281500d8911406f5cc7b3c8f3e6bb0
si-yao <siyao.li.szeyiu@gmail.com>
2015-02-10 22:11:56 -0500

    'auto'

--- Assignment1/solutionsA.py
+++ Assignment1/solutionsA.py
@@ -129,7 +129,7 @@ def linearscore(unigrams, bigrams, trigrams, brown):
             tri_tuple = tuple([tokens[i-2],tokens[i-1],tokens[i]])
             bi_tuple = tuple([tokens[i-1],tokens[i]])
             uni_tuple = tuple([tokens[i]])
-            if(!(tri_p in trigrams)&&!(bi_p in bigrams)&&!(uni_p in unigrams)):#when find a new occurrence, return -1000 for the sentence.
+            if(not(tri_p in trigrams) and not(bi_p in bigrams)and not(uni_p in unigrams)):#when find a new occurrence, return -1000 for the sentence.
                 s = -1000
                 break
             tri_p = 2.0** trigrams.get(tri_tuple,-1000)
