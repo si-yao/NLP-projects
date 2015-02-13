df6016ce166c7582eef7cb3210787b955c2c53d7
si-yao <siyao.li.szeyiu@gmail.com>
2015-02-13 11:16:43 -0500

    vertibi alg

--- Assignment1/solutionsB.py
+++ Assignment1/solutionsB.py
@@ -119,16 +119,20 @@ def q4_output(evalues):
 #tagged is a list of tagged sentences in the format "WORD/TAG". Each sentence is a string with a terminal newline, not a list of tokens.
 def viterbi(brown, taglist, knownwords, qvalues, evalues):
     tagged = []
-    for sentWords in brown:
-        taggedWords = viterbilet(sentWords, taglist, qvalues, evalues)
-        print "taggedWords:"
-        print taggedWords
-        tagged.append(taggedWords)
+    for sentWords_raw in brown:
+	sentWords_raw = ["*", "*"] + sentWords_raw + ["STOP"]
+	sentWords = sentWords_raw[:]
+	for i, w in enumerate(sentWords):
+            if(not(w in knownwords)):
+                sentWords[i] = "_RARE_"
+        taggedWords = viterbilet(sentWords, sentWords_raw, taglist, qvalues, evalues)
+        tagged.append(taggedWords[2:-1])
+        #print tagged
     return tagged
 
 
-def viterbilet(sentWords, taglist, qvalues, evalues):
-    print "TAGLIST:", taglist
+def viterbilet(sentWords, sentWords_raw, taglist, qvalues, evalues):
+    #print "TAGLIST:", taglist
     taggedWords = []
     m = len(taglist) # number of tags
     n = len(sentWords) # number of words
@@ -138,15 +142,14 @@ def viterbilet(sentWords, taglist, qvalues, evalues):
     #for the i-th word, if the tag is tag[j], and previous word is tag[k], 
     #then the best prob now for [anyBestTags],word[i-1]/tag[k],word[i]/tag[j] is A[j][i][k]
     #and D[j][i][k] indicates the best tag for word[i-2], the tag 2 behind i-th word
-    A = m*[n*[m*[0]]] 
-    D = m*[n*[m*[0]]]
+    A = [[[0]*m for ii in range(0,n)] for jj in range(0,m)]
+    D = [[[0]*m for ii in range(0,n)] for jj in range(0,m)]
     for i, word in enumerate(sentWords):# each word
         if(i < 2):
             for j in range(0,m):
                 for k in range(0,m):
                     A[j][i][k] = 0
             continue
-        print A[0]
         for j in range(0,m):# index of current tag for this word
             tag = taglist[j]# current tag
             for k in range(0,m):# index of tag 1 behind
@@ -166,20 +169,22 @@ def viterbilet(sentWords, taglist, qvalues, evalues):
                         maxtag = kk
                 A[j][i][k] = maxi
                 D[j][i][k] = maxtag
-            print A[j]
+
     curmax = -100000000000000
     curmaxI = 0
     prevI = 0
     # for the last word, find the best 1 behind tag, and best 2 behind tag
     for j in range(0,m):
         for k in range(0,m):
-            if(A[j][n-1][k] > curmax):
+            tri_tuple = tuple([taglist[k], taglist[j], "STOP"])
+            if(A[j][n-2][k]+qvalues.get(tri_tuple, -1000) > curmax):
+		curmax = A[j][n-2][k]+qvalues.get(tri_tuple, -1000)
                 curmaxI = j
                 prevI = k
     #print A
-    revTagList = [taglist[curmaxI], taglist[prevI]]
+    revTagList = ["STOP", taglist[curmaxI], taglist[prevI]]
     #for each loop, using curmaxI and prevI, we could find the previous 1 best tag.
-    for i in range(n-1,3,-1):
+    for i in range(n-2,3,-1):
         tmp = D[curmaxI][i][prevI]
         #print "curmaxI:", curmaxI, " i:", i, " prevI:", prevI
         #print "D = ", tmp
@@ -194,7 +199,7 @@ def viterbilet(sentWords, taglist, qvalues, evalues):
 
     #form the tagwords
     #print revTagList
-    for i, word in enumerate(sentWords):
+    for i, word in enumerate(sentWords_raw):
         taggedWords.append(word+"/"+revTagList[i])
     del A
     del D
@@ -246,14 +251,10 @@ def split_wordtags(brown_train):
 
 #Format sentences, and tag rare words as _RARE_.
 #Returns list of list. each ele in the return list is a list of words in one line.
-def formatDev(brown_dev, knownwords):
+def formatDev(brown_dev):
     brown_rst = []
     for sent in brown_dev:
         sentWords = nltk.word_tokenize(sent)
-        sentWords = ["*", "*"] + sentWords + ["STOP"]
-        for i, word in enumerate(sentWords):
-            if(not(word in knownwords)):
-                sentWords[i] = "_RARE_"
         brown_rst.append(sentWords)
     return brown_rst
 
@@ -301,7 +302,7 @@ def main():
     infile.close()
 
     #format Brown development data here
-    brown_dev = formatDev(brown_dev, knownwords)
+    brown_dev = formatDev(brown_dev)
 
     #do viterbi on brown_dev (question 5)
     viterbi_tagged = viterbi(brown_dev, taglist, knownwords, qvalues, evalues)
