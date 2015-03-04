45c38421cb2961151aefebc84053ea4fd4e7b3bf
si-yao <siyao.li.szeyiu@gmail.com>
2015-03-04 00:11:37 -0500

    'auto'

--- Assignment2/public/parse.py
+++ Assignment2/public/parse.py
@@ -1,11 +1,11 @@
 from providedcode.transitionparser import TransitionParser
 from providedcode.dependencygraph import DependencyGraph
-from providedcode.evaluate import DependencyEvaluator
-from featureextractor import FeatureExtractor
-from transition import Transition
+import fileinput
 # parsing arbitrary sentences (english):
-sentence = DependencyGraph.from_sentence('Hi, this is a test')
 
-tp = TransitionParser.load('english.model')
-parsed = tp.parse([sentence])
-print parsed[0].to_conll(10).encode('utf-8')
+for line in fileinput.input():
+
+    sentence = DependencyGraph.from_sentence(line)
+    tp = TransitionParser.load('english.model')
+    parsed = tp.parse([sentence])
+    print parsed[0].to_conll(10).encode('utf-8')
