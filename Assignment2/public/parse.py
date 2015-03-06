35b0ad8040ca1a35c84cf22f1711b2228c238ec6
si-yao <siyao.li.szeyiu@gmail.com>
2015-03-05 23:23:06 -0500

    'auto'

--- Assignment2/public/parse.py
+++ Assignment2/public/parse.py
@@ -1,11 +1,20 @@
 from providedcode.transitionparser import TransitionParser
 from providedcode.dependencygraph import DependencyGraph
 import fileinput
+import sys
 # parsing arbitrary sentences (english):
 
-for line in fileinput.input():
+if __name__ == '__main__':
+    if(len(sys.argv) != 2):
+        print "need 1 argument for model!"
+        exit(1)
 
-    sentence = DependencyGraph.from_sentence(line)
-    tp = TransitionParser.load('english.model')
-    parsed = tp.parse([sentence])
-    print parsed[0].to_conll(10).encode('utf-8')
+    tp = TransitionParser.load(sys.argv[1])
+
+    for line in fileinput.input():
+            sentence = DependencyGraph.from_sentence(line)
+            for node in sentence.nodes:
+                node['ctag'] = '_'
+
+            parsed = tp.parse([sentence])
+            print parsed[0].to_conll(10).encode('utf-8')
