2e66bdd6183fd5bbcdc5a99859fcf3202915a550
si-yao <siyao.li.szeyiu@gmail.com>
2015-03-06 00:08:14 -0500

    'auto'

--- Assignment2/public/parse.py
+++ Assignment2/public/parse.py
@@ -4,7 +4,8 @@ import fileinput
 import sys
 # parsing arbitrary sentences (english):
 import nltk
-from nltk.tag.simplify import simplify_wsj_tag
+from nltk.tag import map_tag
+
 if __name__ == '__main__':
     if(len(sys.argv) != 2):
         print "need 1 argument for model!"
@@ -13,11 +14,10 @@ if __name__ == '__main__':
     tp = TransitionParser.load(sys.argv[1])
     line = sys.stdin.readline()
     while line:
-        tokens = nltk.word_tokenize(line)
-        tagged = nltk.pos_tag(tokens)
+
         sentence = DependencyGraph.from_sentence(line)
-        for (index, (word, tag)) in enumerate(tagged):
-            sentence.nodes[index + 1]['ctag'] = tag
+        for (index, node) in enumerate(sentence.nodes):
+            sentence.nodes[index]['ctag'] = map_tag('en-ptb', 'universal', sentence.nodes[index]['ctag'])
 
         parsed = tp.parse([sentence])
         print parsed[0].to_conll(10).encode('utf-8')
