fd3046933581280ae9ec3d5528b93b39b8b006bb
si-yao <siyao.li.szeyiu@gmail.com>
2015-03-29 23:31:42 -0400

    2

--- Assignment3/Homework3/part2.py
+++ Assignment3/Homework3/part2.py
@@ -16,7 +16,7 @@ def extract_train_from_lex(lexnode, window):
 	for inst in inst_list:
 		l = inst.getElementsByTagName('context')[0]
 		before = nltk.word_tokenize((l.childNodes[0].nodeValue).replace('\n', ''))
-		after = nktl.word)tokenize((l.childNodes[2].nodeValue).replace('\n', ''))
+		after = nltk.word_tokenize((l.childNodes[2].nodeValue).replace('\n', ''))
 		train_dic = {}
 		for i in range(0,window):
 			if i<len(before):
@@ -29,8 +29,8 @@ def extract_train_from_lex(lexnode, window):
 				train_dic[voc] = train_dic.get(voc,0) + 1
 		datalist.append(train_dic)
 		sense_id = inst.getElementsByTagName('answer')[0].getAttribute('senseid')
-		senslist.append(senseid)
-		ses_set.add(sense_id)
+		senslist.append(sense_id)
+		sens_set.add(sense_id)
 	voca_map = {}
 	train_idx = 0
 	for voc in voca_set:
@@ -55,7 +55,7 @@ def extract_train_from_lex(lexnode, window):
 	#trainlist is 2d array, each row is a vector. taglist is an array, each element corresponding to one row in trainlist.
 	#voca_map: word -> index in vector
 	#sens_map: sens_id -> tag nubmer in taglist
-	return tuple(trainlist, taglist, voca_map, sens_map)
+	return (trainlist, taglist, voca_map, sens_map)
 
 if __name__ == '__main__':
 	xmldoc = minidom.parse('lexnode.xml')
