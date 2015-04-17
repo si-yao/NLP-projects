62195c4bcb7073ecd264c68acc2959178dba94c8
si-yao <siyao.li.szeyiu@gmail.com>
2015-04-17 17:43:24 -0400

    a

--- Assignment4/Homework4/A.py
+++ Assignment4/Homework4/A.py
@@ -2,18 +2,31 @@ import nltk
 
 # TODO: Initialize IBM Model 1 and return the model.
 def create_ibm1(aligned_sents):
+    return IBMModel1(aligned_sents, 10);
 
 # TODO: Initialize IBM Model 2 and return the model.
 def create_ibm2(aligned_sents):
+    return IBMModel2(aligned_sents, 10);
 
 # TODO: Compute the average AER for the first n sentences
 #       in aligned_sents using model. Return the average AER.
 def compute_avg_aer(aligned_sents, model, n):
+    count = 0;
+    for(i in range(0,n)):
+        rst = model.align(aligned_sents[i]);
+        count += rst.alignment_error_rate(aligned_sents[i]);
+    return 1.0*count/n;
 
 # TODO: Computes the alignments for the first 20 sentences in
 #       aligned_sents and saves the sentences and their alignments
 #       to file_name. Use the format specified in the assignment.
 def save_model_output(aligned_sents, model, file_name):
+    fout = open(file_name, 'w');
+    for(i in range(0,20)):
+        rst = model.align(aligned_sents[i]);
+        fout.write(rst.words+"\n");
+        fout.write(rst.mots+"\n");
+        fout.write(rst.alignment+"\n\n");
 
 def main(aligned_sents):
     ibm1 = create_ibm1(aligned_sents)
