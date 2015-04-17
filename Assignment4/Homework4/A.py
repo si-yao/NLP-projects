dcc3cf70a19e27c5600e9b31ed824395be9254d5
si-yao <siyao.li.szeyiu@gmail.com>
2015-04-17 18:10:48 -0400

    a

--- Assignment4/Homework4/A.py
+++ Assignment4/Homework4/A.py
@@ -10,7 +10,7 @@ def create_ibm1(aligned_sents):
 
 # TODO: Initialize IBM Model 2 and return the model.
 def create_ibm2(aligned_sents):
-    return IBMModel2(aligned_sents, 10);
+    return IBMModel2(aligned_sents, 30);
 
 # TODO: Compute the average AER for the first n sentences
 #       in aligned_sents using model. Return the average AER.
