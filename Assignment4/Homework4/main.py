877a2a9f423111d4436ccf5a1c844d138866d031
si-yao <siyao.li.szeyiu@gmail.com>
2015-04-27 16:00:56 -0400

    a

--- Assignment4/Homework4/main.py
+++ Assignment4/Homework4/main.py
@@ -5,6 +5,6 @@ import EC
 
 if __name__ == '__main__':
     aligned_sents = comtrans.aligned_sents()[:350]
-    A.main(aligned_sents)
+    #A.main(aligned_sents)
     B.main(aligned_sents)
     EC.main(aligned_sents)
