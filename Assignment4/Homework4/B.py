716d2671ee32bbd068c04e844162933a2ba41ea8
si-yao <siyao.li.szeyiu@gmail.com>
2015-04-27 17:13:04 -0400

    a

--- Assignment4/Homework4/B.py
+++ Assignment4/Homework4/B.py
@@ -72,10 +72,10 @@ class BerkeleyAligner():
         for e in t_ef:
             for f in t_ef[e]:
                 comp = t_ef_inv[f][e]
-                print("comp is "+ str(comp))
-                print("this is "+ str(t_ef[e][f]))
+                #print("comp is "+ str(comp))
+                #print("this is "+ str(t_ef[e][f]))
                 t_ef_new[e][f] = (t_ef[e][f]+comp)/2.0
-                print("new is "+ str(t_ef_new[e][f]))
+                #print("new is "+ str(t_ef_new[e][f]))
                 total_f[f] += t_ef_new[e][f]
         for e in t_ef:
             for f in t_ef[e]:
